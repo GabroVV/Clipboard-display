acc335ac717d46b5dc5fce0269975c4310be945f
GabroVV <origingabriel3124@gmail.com>
2021-05-21 01:23:39 +0200

    Basic functionality implemented

---
+++ clipboard-insert.js
@@ -0,0 +1,24 @@
+const newLineDiv = document.getElementById("new-line");
+const prevLineDiv = document.getElementById("prev-line");
+
+const observer = new MutationObserver(function DOMMutationHandler(mutationList, observer) {
+   for(const mutation of mutationList){
+       let nodes = mutation.addedNodes;
+       for(let node of nodes) {
+         if(node.tagName === "P") // Find <p> tag added by addon
+           prevLineDiv.innerText = newLineDiv.innerText;
+           newLineDiv.innerText = node.innerText;
+           node.remove(); // Remove <p> tag added by addon
+           break;
+       };
+       
+   }
+}
+)
+
+// Only observe childList mutations
+const config = { attributes: false, childList: true, subtree: false };
+const observerTargetNode = document.body
+observer.observe(observerTargetNode, config)
+
+
