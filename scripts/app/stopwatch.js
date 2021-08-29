6694ba9f4bdf334ca5a356a224f0326c8231d34b
GabroVV <origingabriel3124@gmail.com>
2021-08-30 00:14:50 +0200

    Module test 2

--- scripts/app/stopwatch.js
+++ scripts/app/stopwatch.js
@@ -1,6 +1,6 @@
-//Stopwatch
 let stopwatchRunning = false;
 let interval = null;
+
 function iterateTimer() {
    time++;
    updateTimerView();
@@ -40,4 +40,59 @@ function iterateTimer() {
        toggleTimer();
     }
  }
- export {resetTimer, toggleTimer, updateTimerView, iterateTimer};
+
+ //---------------Attempt to load config from local storage---------------
+function loadLocalStorageConfig(){
+    var fontSize = localStorage.getItem('font-size');
+    var bgHex = localStorage.getItem('bg-hex');
+    var fontHex = localStorage.getItem('font-hex');
+    var font = localStorage.getItem('font');
+    var animationSpeedSaved = localStorage.getItem('animation-speed');
+    var animationTypeSaved = localStorage.getItem('animation-type');
+    var whitespace = localStorage.getItem('whitespace');
+    var timeLocal = localStorage.getItem('time');
+    if(fontSize !== null){
+       setFontSize(fontSize);
+    }
+    if(bgHex !== null){
+       setBackgroundColor(bgHex);
+    }
+    if(fontHex !== null){
+       setFontColor(fontHex);
+    }
+    if(font !== null){
+       $('#font').val(font).trigger('change');
+    } else {
+       $('#font').val("Tahoma:400").trigger('change')
+    }
+    if(animationSpeedSaved !== null){
+       setAnimationSpeed(animationSpeedSaved);
+    }
+    if(animationTypeSaved !== null){
+       setAnimationType(animationTypeSaved);
+    }
+    if(whitespace !== null){
+       showWhitespace = whitespace == 'true';
+    }
+    whitespaceCheckbox.checked = showWhitespace;
+    if(timeLocal !== null){
+       time = timeLocal;
+       updateTimerView();
+    }
+ }
+ 
+ loadLocalStorageConfig();
+ 
+ //---------------Attempt to load previous data from local storage---------------
+ var clipDataRaw = localStorage.getItem('clip-data');
+ if(clipDataRaw !== null && clipDataRaw.length > 0){
+    clipData = JSON.parse(clipDataRaw);
+    moveToFront();
+ }
+ 
+ var charactersFromStorage = localStorage.getItem('char-count');
+ if(charactersFromStorage !== null){
+    characters = JSON.parse(charactersFromStorage);
+    updateCharacterCountDisplay();
+ }
+ updateViewOnDataChange();
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-import {resetTimer, toggleTimer, updateTimerView, iterateTimer} from './app/stopwatch.js';
+function require(script) {
+    $.ajax({
+        url: script,
+        dataType: "script",
+        async: false,           // <-- This is the key
+        success: function () {
+            // all good...
+        },
+        error: function () {
+            throw new Error("Could not load script " + script);
+        }
+    });
+}
+require("scripts/libs/jquery.fontpicker.min.js");
+require("scripts/libs/jscolor.min.js");
+require("scripts/app/clipboard-display.js");
+require("scripts/app/stopwatch.js");
